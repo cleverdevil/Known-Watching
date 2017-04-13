fc394b6130b8987028581b45813a33d61e6aab57
Jonathan LaCour <jonathan@cleverdevil.org>
2017-04-13 20:09:20 +0000

    Properly show the title of the watched item even if a photo isn't posted.

--- templates/default/entity/Watching.tpl.php
+++ templates/default/entity/Watching.tpl.php
@@ -1,35 +1,32 @@
 <article class="h-entry known-watching">
-            
             <div class="p-item h-product">
                                 
-    <?php
-        if ($attachments = $vars['object']->getAttachments()) {
-            foreach ($attachments as $attachment) {
-                $mainsrc = $attachment['url'];
-                if (!empty($vars['object']->thumbnail_large)) {
-                    $src = $vars['object']->thumbnail_large;
-                } else if (!empty($vars['object']->thumbnail)) { // Backwards compatibility
-                    $src = $vars['object']->thumbnail;
-                } else {
-                    $src = $mainsrc;
-                }
-                
-                // Patch to correct certain broken URLs caused by https://github.com/idno/known/issues/526
-                $src = preg_replace('/^(https?:\/\/\/)/', \Idno\Core\site()->config()->getDisplayURL(), $src);
-                $mainsrc = preg_replace('/^(https?:\/\/\/)/', \Idno\Core\site()->config()->getDisplayURL(), $mainsrc);
-                
+                <?php
+                    if ($attachments = $vars['object']->getAttachments()) {
+                        foreach ($attachments as $attachment) {
+                            $mainsrc = $attachment['url'];
+                            if (!empty($vars['object']->thumbnail_large)) {
+                                $src = $vars['object']->thumbnail_large;
+                            } else if (!empty($vars['object']->thumbnail)) { // Backwards compatibility
+                                $src = $vars['object']->thumbnail;
+                            } else {
+                                $src = $mainsrc;
+                            }
+                            
+                            // Patch to correct certain broken URLs caused by https://github.com/idno/known/issues/526
+                            $src = preg_replace('/^(https?:\/\/\/)/', \Idno\Core\site()->config()->getDisplayURL(), $src);
+                            $mainsrc = preg_replace('/^(https?:\/\/\/)/', \Idno\Core\site()->config()->getDisplayURL(), $mainsrc);
+                            
+                            ?>
+                            <a href="<?= $this->makeDisplayURL($mainsrc) ?>"><img src="<?= $this->makeDisplayURL($src) ?>" style="float: left; margin: 0 2em 1em 0; max-width: 150px" class="u-photo"/></a>
+                        <?php
+                        }
+                    }
                 ?>
-                <a href="<?= $this->makeDisplayURL($mainsrc) ?>"><img src="<?= $this->makeDisplayURL($src) ?>" style="float: left; margin: 0 2em 1em 0; width: 150px" class="u-photo"/></a>
                 <h2>
                     Watched <span class="p-name"><?= htmlentities(strip_tags($vars['object']->getTitle()), ENT_QUOTES, 'UTF-8'); ?></span>
                 </h2>
-            
             </div>
-            <?php
-            }
-        }
-    ?>
-            
             <div class="e-content">
                 <?= $this->__(['value' => $vars['object']->body, 'object' => $vars['object']])->draw('forms/output/richtext'); ?>
                 
